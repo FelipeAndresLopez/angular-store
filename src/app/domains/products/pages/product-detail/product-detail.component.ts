fde2be823e8f3982fb6eb97e9961b1c3cf70e5b0
Felipe Andres Lopez <felipe.lopezmoreno@baptisthealth.net>
2024-04-27 10:55:35 -0500

    add product detail page

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, inject, signal } from '@angular/core';
+import { Product } from '@shared/model/product.model';
+import { CartService } from '@shared/services/cart.service';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  @Input() id?: string
+  product = signal<Product | null>(null)
+  cover = signal<string | null>(null)
+  private productService = inject(ProductService)
+  private cartService = inject(CartService)
 
+  ngOnInit() {
+    if (this.id) {
+      this.productService.getProduct(this.id)
+        .subscribe({
+          next: (product) => {
+            this.product.set(product)
+            if (product.images.length > 0) {
+              this.cover.set(product.images[0])
+
+            }
+          },
+          error: () => {
+            console.log('error getting product');
+          }
+        })
+    }
+  }
+
+  setCover(image: string) {
+    this.cover.set(image)
+  }
+
+  addToCart() {
+    this.cartService.addCart(this.product()!)
+  }
 }
